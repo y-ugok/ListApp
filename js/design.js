9d76f7511de041a1e640c3e89126197ccc5d9601
y-ugok <114295071+y-ugok@users.noreply.github.com>
2024-10-25 22:53:12 +0900

    テーマカラーの変更全てに反映

--- js/design.js
+++ js/design.js
@@ -1,27 +1,32 @@
 // ページ読み込み時に保存されたテーマカラーを適用
 window.addEventListener("load", () => {
-  // ローカルストレージからテーマカラーを取得
-
   let savedColor = localStorage.getItem("themeColor");
 
-  // ローカルストレージに値がない場合はデフォルトの色 "#FFC3B7" を設定
   if (!savedColor) {
     savedColor = "#FFC3B7"; // デフォルトの色
   }
-  // テーマカラーを適用
   applyThemeColor(savedColor);
+
+  // ボタンのクリックイベントを追加
+  const updateButton = document.getElementById("update-item-btn");
+  const registerButton = document.getElementById("register-item-btn");
+
+  if (updateButton) {
+    updateButton.addEventListener("click", () => applyThemeColor(savedColor));
+  }
+  if (registerButton) {
+    registerButton.addEventListener("click", () => applyThemeColor(savedColor));
+  }
 });
 
 // == テーマカラーの適用 == //
 function applyThemeColor(color) {
-  // ヘッダーや他の要素に選択されたテーマカラーを適用
-  // IDが "open" のボタンにテーマカラーを適用
   const openButton = document.getElementById("open");
   if (openButton) {
     openButton.style.backgroundColor = color;
   }
   document.querySelector("header").style.backgroundColor = color;
-  // 要素の存在を確認してからスタイルを適用
+
   const registerItemBtn = document.getElementById("register-item-btn");
   if (registerItemBtn) {
     registerItemBtn.style.backgroundColor = color;
@@ -36,21 +41,22 @@ function applyThemeColor(color) {
   if (updateButton) {
     updateButton.style.backgroundColor = color;
   }
+
   const completeButtons = document.getElementsByClassName("complete-btn");
-  if (completeButtons) {
-    Array.from(completeButtons).forEach((btn) => {
-      btn.style.backgroundColor = color;
-    });
-  }
+  Array.from(completeButtons).forEach((btn) => {
+    btn.style.backgroundColor = color;
+  });
+
   const rectangles = document.querySelectorAll(".rectangle");
-  const removeButtons = document.querySelectorAll(".remove-btn");
   rectangles.forEach((rect) => {
     rect.style.backgroundColor = color;
   });
+
+  const removeButtons = document.querySelectorAll(".remove-btn");
   removeButtons.forEach((btn) => {
     btn.style.backgroundColor = color;
   });
-  // addStyleクラスの下線の色を変更
+
   const addStyleElements = document.querySelectorAll(".addStyle");
   addStyleElements.forEach((elem) => {
     elem.style.borderBottomColor = color; // 下線の色をテーマカラーに設定
