11cfafc9ced4f945c21ef72cd74efe5711feb3d5
y-ugok <114295071+y-ugok@users.noreply.github.com>
2024-10-31 12:50:26 +0900

    Update history.js

--- js/history.js
+++ js/history.js
@@ -9,13 +9,11 @@ function loadHistory() {
   storedItems.forEach((item) => {
     const li = document.createElement('li');
     li.innerHTML = `
-    <li>
       <div class="list-flex">
         <div class="list-content"><span class="text">${item.action}</span>         
         </div>
         <span class="time">${item.date}</span>
       </div>
-    </li>
     `;
     ul.appendChild(li);
   });
